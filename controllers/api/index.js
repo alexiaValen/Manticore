6e262cd5701775a43c805e42ddf1d46d454556e0
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-08-13 10:13:41 -0700

    bye bye file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const parentRoutes = require('./parentRoutes');
+
 
 router.use('/users', userRoutes);
-router.use('/parents', parentRoutes);
+
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+
 const loginFormHandler = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     event.preventDefault();
@@ -19,11 +20,28 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         document.location.replace('/');
-      }  else {
-        alert('failed to log in'); 
+      } 
+      
+      if (client) {
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: token,
+              audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          // If request specified a G Suite domain:
+          // const domain = payload['hd'];
+        }
+        verify().catch(console.error);
+      }else {
+        alert('Failed to log in');
       }
-   };
-}
+    }
+  };
+
  
   
   document
