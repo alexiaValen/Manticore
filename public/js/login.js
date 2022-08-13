b6777a9e2eb7289816f12a8f3eb77ee35981d6c6
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-08-12 18:07:35 -0700

    Setting up Google library correctly

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,28 @@
+const visibitityBtn = document.getElementById('visibilityBtn');
+const visbitityBtn2 = document.getElementById('visibilityBtn2');
+
+visibitityBtn.addEventListener('click', toggleOn)
+visbitityBtn2.addEventListener('click', toggleOn)
+
+toggleOn = () => {
+    const passwordInput = document.getElementById('password');
+    const passwordConfirmInput = document.getElementById('passwordConfirm');
+    const eye1 = document.getElementById('eye1');
+    const eye2 = document.getElementById('eye2');
+
+    if (passwordInput.type || passwordConfirmInput === 'password') {
+        passwordInput.type = 'text';
+        eye1.innerText = 'visibility_off';
+    } else {
+        passwordInput.type = 'password';
+        passwordConfirmInput = 'password';
+        eye1.innerText = 'visibility';
+        eye2.innerText = 'visibility';
+    }
+} 
+
+
+
 const loginFormHandler = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     event.preventDefault();
