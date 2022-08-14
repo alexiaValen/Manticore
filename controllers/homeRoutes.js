4c8a0326de7fc9b69377daa5cebffa8ec854627b
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-08-14 14:13:28 -0700

    had to update router to work again

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,7 +12,7 @@ router.get('/', withAuth, async (req, res) => {
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('login', {
+    res.render('homepage', {
       users,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
@@ -22,7 +22,7 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/homepage', (req, res) => {
+router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect('/');
